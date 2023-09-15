abd0447e9c8884c5e191bcfc65bbfcf159283de8
aswnss <aswinlalsct@gmail.com>
2023-09-15 12:43:58 +0530

    cart to checkout done

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,47 +1,150 @@
 import {
     Card,
     Image,
-    Box,
     Heading,
     Stack,
-    CardBody,
     Text,
-    CardFooter,
+    Divider,
     Button,
-    Flex
+    Flex,
+    Spacer
 } from '@chakra-ui/react'
+import {useNavigate} from 'react-router-dom'
 import {useEffect, useState} from 'react'
-import { products } from '../assets'
+import {products} from '../assets'
+
+
 function CartPage() {
+    const nav = useNavigate()
     const [cart, setCart] = useState(sessionStorage.getItem('cart') ? JSON.parse(sessionStorage.getItem('cart')) : [])
-    const[cartProducts, setCartProducts] = useState([])
+    const [cartProducts, setCartProducts] = useState([])
+
+    // load product details of cart item
     useEffect(() => {
-        sessionStorage.setItem('cart', JSON.stringify(cart))
-        cart.map((item) => {
-            const found = products.find((product) => product.id === item.id)
-            setCartProducts([...cartProducts, found])
+        const cartProducts = []
+        products.forEach(product => {
+            cart.forEach(item => {
+                if (product.id === item.id) {
+                    cartProducts.push({
+                        ...product,
+                        quantity: item.quantity
+                    })
+                }
+            })
         })
-    }, [cart])
-    console.log(cartProducts)
+        setCartProducts(cartProducts)
+    }, [])
+
+    // handle quantity change
+    // TODO: Remove item from cart if quantity is 0
+    const handleQuantity = (id, add) => {
+        const newCart = cartProducts.map((item) => {
+            if (item.id === id) {
+                if (add) {
+                    item.quantity += 1
+                } else if (item.quantity > 0) {
+                    item.quantity -= 1
+                }
+            }
+            return item
+        })
+        setCartProducts(newCart)
+    }
+    
+    // Calcualte the cost
+    const [cost, setCost] = useState(0)
+    useEffect(() => {
+        let cost = 0
+        cartProducts.forEach(item => {
+            console.log(item.name, item.quantity, item.price)
+            cost += item.quantity * item.price
+        })
+        setCost(cost)
+    }, [cartProducts])
+
+
     return (
-        <Flex direction={'row'} wrap={'wrap'} gap={10}>
-         {
+        <Flex direction={'row'}
+            wrap={'wrap'}
+            justifyContent={'center'}
+            gap={10}
+            py={10}>
+            {
             cartProducts.map((product) => {
                 return (
-                <Card key={product.id} maxW='sm' borderRadius='lg' overflow='hidden'  direction={{ base: 'column', sm: 'row' }}>
-                    <Box boxSize={'sm'}>
-                    <Image src={product.Images.std} alt={product.name} />
-                    </Box>
-                    <Stack>
-                        <Heading size={'sm'}>{product.name}</Heading>
-                        <Text>{product.category}</Text>
-                        <Text>{product.price}</Text>
-                        <Button colorScheme='teal'>Buy Now</Button>
-                    </Stack>
-                  </Card>
+                    <Card key={
+                            product.id
+                        }
+                        w={'95%'}
+                        maxW={1200}
+                        borderRadius='lg'
+                        overflow='hidden'
+                        direction={
+                            {
+                                base: 'column',
+                                sm: 'row'
+                            }
+                        }
+                        gap={'1em'}
+                        alignItems={'center'}>
+
+                        <Image w={'10em'}
+                            src={
+                                product.Images.std
+                            }
+                            alt={
+                                product.name
+                            }/>
+
+                        <Stack>
+                            <Heading size={'sm'}>
+                                {
+                                product.name
+                            }</Heading>
+                            <Text>{
+                                product.category
+                            }</Text>
+                        </Stack>
+
+                        <Spacer/>
+
+                        <Stack mr={'1em'}
+                            w={'8em'}>
+                            <Text>${
+                                product.price
+                            }</Text>
+                            <Flex alignContent={'center'}
+                                placeItems={'center'}
+                                gap={'1em'}>
+                                <Button onClick={
+                                    () => {
+                                        handleQuantity(product.id, false)
+                                    }
+                                }>-</Button>
+                                <Text>{
+                                    product.quantity
+                                }</Text>
+                                <Button onClick={
+                                    () => {
+                                        handleQuantity(product.id, true)
+                                    }
+                                }>+</Button>
+                            </Flex>
+                        </Stack>
+                    </Card>
                 )
             })
-         }
+        }
+            <Divider/>
+            <Flex direction={'column'}
+                gap={5}
+                w={'95%'}
+                maxW={1200}>
+                <Heading size={'md'}>Total Cost: ${cost}</Heading>
+                <Button colorScheme={'teal'} onClick={()=>{
+                    nav('/checkout', {state: {cartProducts, cost}})
+                }}>Checkout</Button>
+            </Flex>
         </Flex>
     )
 }
--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
+import { useLocation,useNavigate } from "react-router-dom"
+import { Container,VStack,Heading,Text,Button,Input,Textarea, Divider, Flex,Spacer } from "@chakra-ui/react";
 function Checkout() {
+  const products = useLocation().state.cartProducts;
+  const cost = useLocation().state.cost;
+  if(!products || !cost) {
+    const nav = useNavigate()
+    nav('/')
+  }
+
   return (
     <div>
-      Checkout
+      <Container my={'1em'}>
+        <Heading>Checkout</Heading>
+        <VStack spacing={4} mt={'1em'}>
+          <Text>Enter your details</Text>
+          <Input placeholder={'Name'} />
+          <Input placeholder={'Email'} />
+          <Input placeholder={'Phone'} />
+          <Textarea placeholder={'Address'} />
+          <Button colorScheme={'teal'}>Place Order</Button>
+        </VStack>
+      <Divider my={'2em'}/> 
+        <Heading fontSize={'md'}>Products</Heading>
+        <Flex spacing={4} mt={'1em'} alignItems={'space-evenly'} flexDirection={'column'} gap={'1em'}>
+          {
+            products.map((item) => {
+              return (
+              <Flex key={item.id} >
+                  <Text>{item.name} x {item.quantity}</Text>
+                  <Spacer />
+                  <Text>{item.price}</Text>
+                </Flex>
+                  )
+                })
+          }
+        </Flex>
+          <Divider my={'1em'} />
+          <Flex>
+            <Text fontWeight={'bold'}>Total</Text>
+            <Spacer />
+            <Text fontWeight={'bold'}>{cost}</Text>
+          </Flex>
+
+      </Container>
     </div>
   )
 }
