0f0527d60c86dc78f94823c8a8d8acc253860faa
aswnss <aswinlalsct@gmail.com>
2023-09-15 09:58:06 +0530

    function added

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
-
+import {
+    Card,
+    Image,
+    Box,
+    Heading,
+    Stack,
+    CardBody,
+    Text,
+    CardFooter,
+    Button,
+    Flex
+} from '@chakra-ui/react'
+import {useEffect, useState} from 'react'
+import { products } from '../assets'
 function CartPage() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [cart, setCart] = useState(sessionStorage.getItem('cart') ? JSON.parse(sessionStorage.getItem('cart')) : [])
+    const[cartProducts, setCartProducts] = useState([])
+    useEffect(() => {
+        sessionStorage.setItem('cart', JSON.stringify(cart))
+        cart.map((item) => {
+            const found = products.find((product) => product.id === item.id)
+            setCartProducts([...cartProducts, found])
+        })
+    }, [cart])
+    console.log(cartProducts)
+    return (
+        <Flex direction={'row'} wrap={'wrap'} gap={10}>
+         {
+            cartProducts.map((product) => {
+                return (
+                <Card key={product.id} maxW='sm' borderRadius='lg' overflow='hidden'  direction={{ base: 'column', sm: 'row' }}>
+                    <Box boxSize={'sm'}>
+                    <Image src={product.Images.std} alt={product.name} />
+                    </Box>
+                    <Stack>
+                        <Heading size={'sm'}>{product.name}</Heading>
+                        <Text>{product.category}</Text>
+                        <Text>{product.price}</Text>
+                        <Button colorScheme='teal'>Buy Now</Button>
+                    </Stack>
+                  </Card>
+                )
+            })
+         }
+        </Flex>
+    )
 }
 
 export default CartPage
